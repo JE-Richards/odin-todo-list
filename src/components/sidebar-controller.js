d8517a93721a16dc26d594b80f0c87a0e853e0ad
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-07-05 09:20:40 +0100

    Add checkbox to todo display to toggle complete
    Each todo will now be displayed with an accompanying checkbox to toggle
    whether the todo has been completed or not. Added functionality to this
    checkbox to alter the class list of the to-do's parent div container so
    that we can alter the styling of the to-do depending on whether it is
    complete or not.

--- src/components/sidebar-controller.js
+++ src/components/sidebar-controller.js
@@ -1,4 +1,5 @@
 import { Workspace, WorkspaceManager } from "./workspaces";
+import { Todo } from "./todo";
 import { prepopulateWorkspaceEdit } from "./prepopulate-edit-forms";
 
 function renderWorkspacesNav(array) {
@@ -48,16 +49,34 @@ function populateWorkspaceDisplay(workspace, workspaceTodoList) {
 
     workspaceTodoList.forEach(item => {
         const todoDiv = document.createElement('div');
+        const todoCheckbox = document.createElement('input');
         const todoTitle = document.createElement('h1');
         const todoDescription = document.createElement('p');
         const todoDueDate = document.createElement('p');
         const todoPriority = document.createElement('p');
 
+        todoDiv.dataset.todo = item.title;
         todoTitle.innerHTML = item.title;
         todoDescription.innerHTML = item.description;
         todoDueDate.innerHTML = item.dueDate;
         todoPriority.innerHTML = item.priority;
 
+        todoCheckbox.type = 'checkbox';
+        todoCheckbox.checked = item.isComplete;
+        todoCheckbox.addEventListener('change', (event) => {
+            let thisTodoDiv = event.target.closest('div');
+            let thisTodoTitle = thisTodoDiv.getAttribute('data-todo');
+            let thisTodoList = WorkspaceManager.currentWorkspace.getTodoList();
+            let idx = thisTodoList.findIndex(item => item.title === thisTodoTitle);
+
+            // toggle whether todo is complete
+            Todo.toggleComplete(thisTodoList[idx]);
+
+            // add a complete class to the todo div for use in styling
+            (thisTodoList[idx].isComplete === true) ? (thisTodoDiv.classList.add('complete')) : (thisTodoDiv.classList.remove('complete'));
+        })
+
+        todoDiv.appendChild(todoCheckbox);
         todoDiv.appendChild(todoTitle);
         todoDiv.appendChild(todoDescription);
         todoDiv.appendChild(todoDueDate);
